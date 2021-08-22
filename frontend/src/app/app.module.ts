3ac5253a20a6777f2f15ebe1fa0dedc0fa9f2f4d
Felipe Restrepo <mfrestrepo@superservicios.gov.co>
2021-08-21 19:36:14 -0500

    Frontend boilerplate

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { EmployeeService } from './employee.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule, FormsModule
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
